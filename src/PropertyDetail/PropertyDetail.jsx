1729622157b3b9c9861e6f19faf9fc4b50bf2363
metatony <ibegbulam.ua@gmail.com>
2025-08-12 15:39:59 +0100

    Enhance image gallery in PropertyDetail for improved styling and responsiveness

--- src/PropertyDetail/PropertyDetail.jsx
+++ src/PropertyDetail/PropertyDetail.jsx
@@ -27,18 +27,18 @@ const PropertyDetail = () => {
       />
 
       {/* image gallery */}
-      <section className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
+      <section className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6 bg-red-300">
         <div className="lg:col-span-2 w-full">
           <img
             src={house.desktopImageUrl}
             alt={house.title}
-            className="w-full lg:h-[592px]"
+            className="w-full lg:h-[592px] object-cover"
             loading="lazy"
           />
         </div>
-        <div className="lg:col-span-1 grid lg:grid-cols-1 gap-6 justify-between items-center">
-          <img src={imageGallery1} alt={house.title} className="w-full bg-cover" loading="lazy" />
-          <img src={imageGallery2} alt={house.title} className="w-full bg-cover" loading="lazy" />
+        <div className="lg:col-span-1 grid lg:grid-cols-1 gap-6 justify-between items-center lg:h-[592px] ">
+          <img src={imageGallery1} alt={house.title} className="w-full h-full object-cover" loading="lazy" />
+          <img src={imageGallery2} alt={house.title} className="w-full h-full object-cover" loading="lazy" />
         </div>
       </section>
 
