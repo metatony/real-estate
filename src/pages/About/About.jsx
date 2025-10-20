0931fb93dcc3d26fab4bf06c9e6481a2b7bed1f4
metatony <ibegbulam.ua@gmail.com>
2025-10-20 13:10:07 +0100

    Refactor About component for improved layout; change to flex column for better alignment and update link styles for consistency.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,16 +2,28 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="container mx-auto px-4 flex h-[35vh] justify-center items-center font-light">
+    <div className="container mx-auto px-4 flex flex-col h-[35vh] justify-center items-center font-light">
       <p className="text-center">
-        Thanks for checking me out 😉 <br /> GGs -{" "}
+        Thanks for checking me out 😉 <br /> GitHub - {" "}
         <a
-          className="underline text-[#7e7d7d] hover:text-black"
+          className="text-blue-500"
           target="_blank"
           href="https://github.com/metatony"
           rel=" noopener noreferrer "
         >
           metatony
+        </a>
+      </p>
+
+      <p className="text-center">
+        View my blog - {" "}
+        <a
+          className="text-blue-500"
+          target="_blank"
+          href="https://mlog-alpha.vercel.app/"
+          rel=" noopener noreferrer "
+        >
+          metatony blog
         </a>
       </p>
     </div>
